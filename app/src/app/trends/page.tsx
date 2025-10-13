db65d7f58be78e29e7f4b3cc1916a2f67dfe05f0
iwillreku3206 <37181282+iwillreku3206@users.noreply.github.com>
2025-10-14 00:36:36 +0800

    i hate u jarvivivivivivivis

--- app/src/app/trends/page.tsx
+++ app/src/app/trends/page.tsx
@@ -2,9 +2,5 @@
 import Header from "@/components/Header";
 
 export default function Trends() {
-  return (
-    <div className="flex min-h-screen flex-col bg-white">
-      <Header />
-    </div>
-  );
+  return <div className="flex min-h-screen flex-col"></div>;
 }
