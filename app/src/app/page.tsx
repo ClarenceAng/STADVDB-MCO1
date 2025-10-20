e313a27252bc26ecf4713336c8bc5cfd9cb6eb20
iwillreku3206 <37181282+iwillreku3206@users.noreply.github.com>
2025-10-20 23:11:50 +0800

    update app

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -11,38 +11,7 @@ export default function Home() {
       className="flex flex-col items-center justify-center"
       style={{ height: "calc(100vh - 88px)" }}
     >
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-md">
-        <Link
-          href="/audience"
-          className="rounded-md bg-green-800 px-8 py-2 text-center text-xl"
-        >
-          Audience
-        </Link>
-        <Link
-          href="/genre"
-          className="rounded-md bg-green-800 px-8 py-2 text-center text-xl"
-        >
-          Genres
-        </Link>
-        <Link
-          href="/ratings"
-          className="rounded-md bg-green-800 px-8 py-2 text-center text-xl"
-        >
-          Trends
-        </Link>
-        <Link
-          href="/trends"
-          className="rounded-md bg-green-800 px-8 py-2 text-center text-xl"
-        >
-          Ratings
-        </Link>
-        <Link
-          href="/revenue"
-          className="rounded-md bg-green-800 px-8 py-2 text-center text-xl"
-        >
-          Revenue
-        </Link>
-      </div>
+      Click the menu on the top left to view pages
     </div>
   );
 }
